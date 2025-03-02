362369a7cd88641fe7d13194c718acf8e431d28d
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2025-03-02 21:50:30 +0900

    [Comment] 로직 아이디어에 대한 주석 추가
    설정의 기존값을 확인하는 로직에 필요한 부연설명을 주석으로 추가

--- fail2ban_setup.sh
+++ fail2ban_setup.sh
@@ -5,7 +5,7 @@ SSH_PORT=$1
 # 1. fail2ban 설치
 
 
-# 2. jail.conf 파일의 사본(jail.local) 만들기
+# 2. jail.conf 파일의 사본(jail.local) 만들기, jail.local이 이미 있는 경우 무시
 
 
 # 3. 사본에서 sshd 설징 부분을 찾아, 제한 설정
--- ssh_setup.sh
+++ ssh_setup.sh
@@ -4,12 +4,16 @@ SSHD_CONFIG="/etc/ssh/sshd_config"
 
 
 # 1. ssh 포트 변경
+# 현재의 포트 번호 확인 후, 변경(설정파일에 값 쓰인 방식 다름)
 sudo sed -i "s/^#\?Port [0-9]\+/Port $SSH_PORT/" $SSHD_CONFIG
 
 # 2. 비밀번호 사용 로그인 금지
+# 이미 금지되었다면 무시하고, 넘어가기
 sudo sed -i "s/^#\?PasswordAuthentication .*/PasswordAuthentication no/" $SSHD_CONFIG
 
 # 3. 공개키 사용 로그인 허가
+# 이미 금지되었다면 무시하고, 넘어가기
 sudo sed -i "s/^#\?PubkeyAuthentication .*/PubkeyAuthentication yes/" $SSHD_CONFIG
 
+# 4. 설정을 변경했는 지 확인하고, 변경한 경우에 한하여 재시작ㄱㄱ
 sudo systemctl restart ssh
